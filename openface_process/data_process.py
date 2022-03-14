f75ccf2be9b40404d6c48783b42bbb2a7710654b
AIlinx <45188056+JYDbeg@users.noreply.github.com>
2022-03-15 00:57:31 +0900

    Update data_process.py

--- openface_process/data_process.py
+++ openface_process/data_process.py
@@ -1,10 +1,7 @@
 import subprocess
 import numpy as np
-id_name = np.arange(1,41,1)
-for i in id_name:
-    print(str(i).zfill(3))
-    
-root_path = "D:/BaiduNetdiskDownload/py/OpenFace_2.2.0_win_x64/FaceGrabberDB/"
+id_name = np.arange(1,41,1)  
+root_path = "Disk:/Somedirectory/"
 emotion=["01_HeadScan","02_Anger","03_Disgust","04_Fear","05_Happiness","06_Sadness","07_Surprise","08_Random"]
 subdir = "KinectColorRaw"
 for i in id_name:
